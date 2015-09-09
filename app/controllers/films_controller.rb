15c5d952f68ff01c23251a0b9db769f9e49cf527
JulienPuchol <julienpuchol@gmail.com>
2015-09-09 15:12:47 +0200

    Test après MAJ canal+

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,13 +1,95 @@
 require 'mechanize'
 
+
+
 class FilmsController < ApplicationController
   def get1
   	
   	Film.delete_all
 	agent = Mechanize.new
 	agent2 = Mechanize.new
-	agent3 = Mechanize.new
 
+	j = 1
+	page = agent.get('http://www.canalplus.fr/c-cinema/pid6268-les-films-sur-canal.html?page='+j.to_s+'&onglet=1')
+	while (page.links[67].text.eql?('Toutes les émissions cinéma')==false) 
+	  
+	  i=67
+	  
+	  while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
+	    infos = page.links[i].text().split("Court métrage")[0]
+	                                .split("Film x")[0]
+	                                .split("Peplum")[0]
+	                                .split("Téléfilm")[0]
+	                                .split("Téléfilm d'animation")[0]
+	                                .split("Action")[0]
+	                                .split("Fantastique")[0]
+	                                .split("Film historique")[0]
+	                                .split("Science-fiction")[0]
+	                                .split("Thriller")[0]
+	                                .split("Film d'animation")[0]                                
+	                                .split("Aventures")[0]
+	                                .split("Film biographique")[0]
+	                                .split("Comédie")[0]
+	                                .split("Comédie dramatique")[0]
+	                                .split("Horreur")[0]
+	                                .split("Comédie sentimentale")[0]
+	                                .split("Documentaire")[0]
+	                                .split("Drame")[0]
+	                                .split("Film de guerre")[0]
+	                                .split("Western")[0]
+	                                .split("NC")
+
+
+
+
+	    film = agent2.get('http://www.allocine.fr/recherche/?q='+infos[0])
+	    link = 0
+	    name = infos[0].strip!
+	    if name != nil 
+	      infos[0] = name.split(" ").join("+")
+	    end
+	    
+	    
+	    
+	    while (film.links[link] != nil && "".eql?(film.links[link].text())==false) 
+	      link = link + 1
+	    end
+
+	    if (film.links[link] == nil) 
+	      notePresse = "NC"
+	      noteSpectateurs = "NC"
+	      link_allocine = "NC"
+	    else 
+	      link_allocine = film.links[link+1].uri()
+	      film = film.links[link+1].click
+	      senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
+
+
+
+	      for k in 0..1 
+	        unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
+	          tab = search.split('>') unless search.nil?
+	          until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
+	            tab.delete_at(0)
+	          end
+	          if k==0
+	            notePresse = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
+	          else 
+	            noteSpectateurs = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
+	          end
+	        end
+	      end
+	      Film.film!(name,notePresse,noteSpectateurs,link_allocine,senscritique)
+	      
+	    end
+	    puts "Note presse : ".concat(notePresse)
+	    puts "Note spectateurs : ".concat(noteSpectateurs)
+	    puts "Lien allocine : ".concat(link_allocine.to_s)
+	    i=i+2
+	  end
+	  j=j+1
+	  page = agent.get('http://www.canalplus.fr/c-cinema/pid6268-les-films-sur-canal.html?page='+i+'&onglet=1')
+	end
 	j=1
 	
 	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
@@ -23,64 +105,7 @@ class FilmsController < ApplicationController
 			          	unless film.links_with(:href => /film/)[1].nil?
 				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
 				            film = film.links_with(:href => /film/)[1].click
-		            		senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
-				            for k in 0..1 
-				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
-					                tab = search.split('>') unless search.nil?
-					                until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
-					                  tab.delete_at(0)
-					                end
-					                if k==0
-					            	  	press = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	else 
-					              	spectator = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	end
-				          		end
-				        	end
-
-				        	Film.film!(film.title(),press,spectator,link,senscritique)
-
-			        
-			    		end
-
-			    	end
-		  		end
-			end
-		i=i+1
-		end
-	
-
-  	flash[:success]="Films 1 added!"
-  	redirect_to get2_path
-  end
-
-  def get2
-  	
-  	
-	agent = Mechanize.new
-	agent2 = Mechanize.new
-
-
-	j=2
-	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
-
-	  	i=30
-	  
-	  	while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
-		    if page.links[i].text().eql?('')==false
-		      	if page.links[i].text().eql?('BA')==false
-			        if page.links[i].text().eql?('Enregistrement')==false
-			          
-			          film = agent2.get('http://www.allocine.fr/recherche/?q='+page.links[i].text().split(' ').join("+"))
-
-			          	unless film.links_with(:href => /film/)[1].nil?
-				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
-				            film = film.links_with(:href => /film/)[1].click
-				            senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
+		            		
 
 				            for k in 0..1 
 				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
@@ -108,288 +133,11 @@ class FilmsController < ApplicationController
 		end
 	
 
-  	flash[:success]="Films 1 added!"
-  	redirect_to get3_path
-  end
-
-  def get3
-  	
-  	
-	agent = Mechanize.new
-	agent2 = Mechanize.new
-
-	j=3
-	
-	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
-
-	  	i=30
-	  
-	  	while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
-		    if page.links[i].text().eql?('')==false
-		      	if page.links[i].text().eql?('BA')==false
-			        if page.links[i].text().eql?('Enregistrement')==false
-			          
-			          film = agent2.get('http://www.allocine.fr/recherche/?q='+page.links[i].text().split(' ').join("+"))
-
-			          	unless film.links_with(:href => /film/)[1].nil?
-				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
-				            film = film.links_with(:href => /film/)[1].click
-				            senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
-
-				            for k in 0..1 
-				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
-					                tab = search.split('>') unless search.nil?
-					                until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
-					                  tab.delete_at(0)
-					                end
-					                if k==0
-					            	  	press = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	else 
-					              	spectator = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	end
-				          		end
-				        	end
-
-				        	Film.film!(film.title(),press,spectator,link,senscritique)
-
-			        
-			    		end
-
-			    	end
-		  		end
-			end
-		i=i+1
-		end
-	
-
-  	flash[:success]="Films 1 added!"
-  	redirect_to get4_path
-  end
-
-  def get4
-  	
-  	
-	agent = Mechanize.new
-	agent2 = Mechanize.new
-
-	j=4
-	
-	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
-
-	  	i=30
-	  
-	  	while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
-		    if page.links[i].text().eql?('')==false
-		      	if page.links[i].text().eql?('BA')==false
-			        if page.links[i].text().eql?('Enregistrement')==false
-			          
-			          film = agent2.get('http://www.allocine.fr/recherche/?q='+page.links[i].text().split(' ').join("+"))
-
-			          	unless film.links_with(:href => /film/)[1].nil?
-				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
-				            film = film.links_with(:href => /film/)[1].click
-				            senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
-
-				            for k in 0..1 
-				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
-					                tab = search.split('>') unless search.nil?
-					                until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
-					                  tab.delete_at(0)
-					                end
-					                if k==0
-					            	  	press = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	else 
-					              	spectator = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	end
-				          		end
-				        	end
-
-				        	Film.film!(film.title(),press,spectator,link,senscritique)
-
-			        
-			    		end
-
-			    	end
-		  		end
-			end
-		i=i+1
-		end
-	
-
-  	flash[:success]="Films 1 added!"
-  	redirect_to get5_path
-  end
-
-  def get5
-  	
-  	
-	agent = Mechanize.new
-	agent2 = Mechanize.new
-
-	j=5
-	
-	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
-
-	  	i=30
-	  
-	  	while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
-		    if page.links[i].text().eql?('')==false
-		      	if page.links[i].text().eql?('BA')==false
-			        if page.links[i].text().eql?('Enregistrement')==false
-			          
-			          film = agent2.get('http://www.allocine.fr/recherche/?q='+page.links[i].text().split(' ').join("+"))
-
-			          	unless film.links_with(:href => /film/)[1].nil?
-				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
-				            film = film.links_with(:href => /film/)[1].click
-				            senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
-
-				            for k in 0..1 
-				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
-					                tab = search.split('>') unless search.nil?
-					                until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
-					                  tab.delete_at(0)
-					                end
-					                if k==0
-					            	  	press = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	else 
-					              	spectator = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	end
-				          		end
-				        	end
-
-				        	Film.film!(film.title(),press,spectator,link,senscritique)
-
-			        
-			    		end
-
-			    	end
-		  		end
-			end
-		i=i+1
-		end
-	
-
-  	flash[:success]="Films 1 added!"
-  	redirect_to get6_path
-  end
-
-  def get6
-  	
-  	
-	agent = Mechanize.new
-	agent2 = Mechanize.new
-
-	j=6
-	
-	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
-
-	  	i=30
-	  
-	  	while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
-		    if page.links[i].text().eql?('')==false
-		      	if page.links[i].text().eql?('BA')==false
-			        if page.links[i].text().eql?('Enregistrement')==false
-			          
-			          film = agent2.get('http://www.allocine.fr/recherche/?q='+page.links[i].text().split(' ').join("+"))
-
-			          	unless film.links_with(:href => /film/)[1].nil?
-				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
-				            film = film.links_with(:href => /film/)[1].click
-				            senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
-				            for k in 0..1 
-				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
-					                tab = search.split('>') unless search.nil?
-					                until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
-					                  tab.delete_at(0)
-					                end
-					                if k==0
-					            	  	press = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	else 
-					              	spectator = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	end
-				          		end
-				        	end
-
-				        	Film.film!(film.title(),press,spectator,link,senscritique)
-
-			        
-			    		end
-
-			    	end
-		  		end
-			end
-		i=i+1
-		end
-	
-
-  	flash[:success]="Films 1 added!"
-  	redirect_to get7_path
-  end
-
-  def get7
-  	
-  	
-	agent = Mechanize.new
-	agent2 = Mechanize.new
-
-	j=7
-	
-	  	page = agent.get('http://www.canalplus.fr/c-grille-tv/pid3248-c-films-du-moment.html?page='+j.to_s())
-
-	  	i=30
-	  
-	  	while (page.links[i].text().eql?('Page Suivante')==false && page.links[i].text().eql?('Page Précédente')==false)
-		    if page.links[i].text().eql?('')==false
-		      	if page.links[i].text().eql?('BA')==false
-			        if page.links[i].text().eql?('Enregistrement')==false
-			          
-			          film = agent2.get('http://www.allocine.fr/recherche/?q='+page.links[i].text().split(' ').join("+"))
-
-			          	unless film.links_with(:href => /film/)[1].nil?
-				            link = "http://www.allocine.fr"+film.links_with(:href => /film/)[1].uri().to_s
-				            film = film.links_with(:href => /film/)[1].click
-				            senscritique = 'http://www.senscritique.com/recherche?query='+page.links[i].text().split(' ').join("+")
-
-
-
-				            for k in 0..1 
-				              	unless (search = film.search(".//span[@class='note']")[k].to_s).nil?
-					                tab = search.split('>') unless search.nil?
-					                until ( tab[0].nil? || tab[0][0,1].eql?("<")==false)
-					                  tab.delete_at(0)
-					                end
-					                if k==0
-					            	  	press = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	else 
-					              	spectator = tab[0].split('<')[0].split(',').join('.') unless tab[0].nil?
-					            	end
-				          		end
-				        	end
-
-				        	Film.film!(film.title(),press,spectator,link,senscritique)
-
-			        
-			    		end
-
-			    	end
-		  		end
-			end
-		i=i+1
-		end
-	
-
-  	flash[:success]="Films 1 added!"
+  	flash[:success]="Films added!"
   	redirect_to root_path
   end
+
+  
 
 
 	def showBestPress
